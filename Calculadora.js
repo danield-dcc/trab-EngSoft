bdc33b563765624d79dd6d8804f1ac91ca074f8f
Daniel Conceição <danieldcc.colares@gmail.com>
2020-11-09 22:33:36 -0300

    :sparkles: Adiciona menu e funcao soma Adiciona um menu simples e a função de soma

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,41 @@
-function sum(){
-    //TODO: Implementar soma
+let prompt = require('prompt-sync')()
+
+
+menu:
+    while(true){
+       
+        console.log('Calculadora')
+        console.log('Qual operação deseja realizar: ')
+        console.log('1.  Soma')
+        console.log('2.  Subtração')
+        console.log('3.  Multiplicação')
+        console.log('4.  Divisão')
+        var opcao = Number(prompt('Opção: '))
+
+            switch(opcao){
+                case 1:
+                    sum()
+                    break
+                case 2:
+                    sub()
+                    break
+                case 3:
+                    mult()
+                    break
+                case 4:
+                    div()
+                    break
+                default:
+                 break menu
+            }
+    }
+
+function sum(){  //TODO: Implementar soma
+    let a = Number(prompt("Primerio Número: ") );
+    let b = Number(prompt("Segundo Número: ") );
+    let res = a + b;
+    console.log(`Resultado: ${res}`)
+
 }
 
 function sub(){
