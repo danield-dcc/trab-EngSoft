b9015e74816205447132da23a572265111f0ccec
Daniel Conceição <danieldcc.colares@gmail.com>
2020-11-11 17:39:21 -0300

    :package: Adiciona branch menu Adiciona branch menu com o código corretamente.

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,38 @@
-function sum(){
-    //TODO: Implementar soma
+let prompt = require('prompt-sync')();
+
+menu:
+    while(true){
+       
+        console.log('Calculadora');
+        console.log('Qual operação deseja realizar: ');
+        console.log('1.  Soma');
+        console.log('2.  Subtração');
+        console.log('3.  Multiplicação');
+        console.log('4.  Divisão');
+        console.log('5.  Sair');
+
+        var opcao = Number(prompt('Opção: '));
+
+            switch(opcao){
+                case 1:
+                    sum();
+                    break
+                case 2:
+                    sub();
+                    break
+                case 3:
+                    mult();
+                    break
+                case 4:
+                    div();
+                    break
+                default:
+                 break menu
+            }
+    }
+
+function sum(){  //TODO: Implementar soma
+    
 }
 
 function sub(){
